7acb91578b8491b5935f8ccdf74154bce74ceb46
Oleg G.Kapranov <lugatex@yahoo.com>
2018-06-13 21:06:18 +0300

    alert - admin,users,show

--- app/routes/admin/users/show.js
+++ app/routes/admin/users/show.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params) {
+    return this.get('store').findRecord('user', params.user_id)
+      .then((user) => {
+        alert(
+          'User: ' + user.get('name') + '\n' + 'Email: ' + user.get('email')
+        )
+      })
+  },
+
+  setupController(controller, model) {
+    controller.set('users', model.user_id);
+    this._super(controller, model);
+  },
+
+  redirect() {
+    this.transitionTo('admin.users.index');
+  }
 });
