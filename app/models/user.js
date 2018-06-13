9f7df4a6a27e189d6e9e67627d836df10538dc8f
Oleg G.Kapranov <lugatex@yahoo.com>
2018-06-13 21:54:12 +0300

    added to model defaulValue

--- app/models/user.js
+++ app/models/user.js
@@ -2,15 +2,21 @@ import DS from 'ember-data';
 import { computed } from '@ember/object';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  email: DS.attr('string'),
-  bio: DS.attr('string'),
-  avatarUrl: DS.attr('string'),
-  spent: DS.attr('dollars'),
+  name: DS.attr('string', { defaultValue: 'Your FullName' }),
+  email: DS.attr('string', { defaultValue: 'Your Mail Box' }),
+  bio: DS.attr('string', { defaultValue: 'Your Biography' }),
+  avatarUrl: DS.attr('string', { defaultValue: 'donald.png' }),
+  spent: DS.attr('dollars', { defaultValue: 0 }),
   updatedAt: DS.attr('number'),
   createdAt: DS.attr('number'),
   roles: DS.hasMany('role', { async: true }),
   createdMonth: computed('createdAt', function(){
     return this.get('createdAt').getMonth() + 1
+  }),
+  //fullInfo: (function(){
+  //  return (this.get('name')) + ' ' + (this.get('email'));
+  //}).property('name', 'email')
+  fullInfo: computed('name', 'email', function(){
+    return (this.get('name')) + ' ' + (this.get('email'));
   })
 });
