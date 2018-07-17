e48739c7a88ebf0e7ab1b3874538bb7b249b7a0e
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 21:01:40 +0300

    updaed component expand-toggle

--- app/components/expand-toggle.js
+++ app/components/expand-toggle.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
 
 export default Component.extend({
+  actions: {
+    collapseRow(index, record) {
+      get(this, 'collapseRow')(index, record);
+    },
+    expandRow(index, record) {
+      get(this, 'expandRow')(index, record);
+    }
+  }
 });
