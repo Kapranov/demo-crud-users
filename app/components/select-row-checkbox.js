daac462b919509b88a64a4ab8af5c2af4a720f42
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 21:23:11 +0300

    updated component select-row-checkbox

--- app/components/select-row-checkbox.js
+++ app/components/select-row-checkbox.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
 
 export default Component.extend({
+  actions: {
+    clickOnRow(index, record, event) {
+      get(this, 'clickOnRow')(index, record);
+      event.stopPropagation();
+    }
+  }
 });
