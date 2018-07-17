46d633ebbe768f0d08dd7830111893715d353805
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 20:51:58 +0300

    edited component delete-row-comp

--- app/components/delete-row-comp.js
+++ app/components/delete-row-comp.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
 
 export default Component.extend({
+  record: null,
+
+  click(event) {
+    let onClick = get(this, 'onClick');
+    if (onClick) {
+      onClick(get(this, 'record'));
+      event.stopPropagation();
+    }
+  }
 });
