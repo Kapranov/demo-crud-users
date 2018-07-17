f7397211c9e31b1bb8040d46346b2dba2cd79b11
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 20:55:02 +0300

    added component delete-row

---
+++ app/components/delete-row.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+
+export default Component.extend({
+  sendAction: null,
+  record: null,
+
+  actions: {
+    sendAction(actionName, record, event) {
+      get(this, 'sendAction')(actionName, record);
+      event.stopPropagation();
+    }
+  }
+});
