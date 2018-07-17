b79a5831cd0fd14c9919c2a51ee818147902e758
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 21:20:42 +0300

    updated component select-all-rows-checkbox

--- app/components/select-all-rows-checkbox.js
+++ app/components/select-all-rows-checkbox.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
 
 export default Component.extend({
+  actions: {
+    toggleAllSelection() {
+      get(this, 'toggleAllSelection')();
+    }
+  }
 });
