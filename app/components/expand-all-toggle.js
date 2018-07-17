6d06375b98b4af7041f20bc5d1b07a291087efb1
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-17 20:57:41 +0300

    added component expand-all-toggle

--- app/components/expand-all-toggle.js
+++ app/components/expand-all-toggle.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
 
 export default Component.extend({
+  actions: {
+    collapseAllRows() {
+      get(this, 'collapseAllRows')();
+    },
+
+    expandAllRows() {
+      get(this, 'expandAllRows')();
+    }
+  }
 });
