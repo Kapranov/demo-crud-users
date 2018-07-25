ce3d9bb65f8fb6e3a87674d48c99fe4f3981efb8
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-25 21:06:22 +0300

    created component test-demo-context-menu

---
+++ app/components/test-demo-context-menu.js
@@ -0,0 +1,62 @@
+import Component from '@ember/component';
+import computed from '@ember/object/computed';
+import demoContextMenu from './demo-context-menu';
+
+function increment(objects, amount = 1) {
+  objects.forEach((object) => {
+    object.incrementProperty('count', amount);
+  });
+}
+
+function setTo(objects, value = 0) {
+  objects.forEach((object) => {
+    object.set('count', value);
+  });
+}
+
+export default Component.extend(demoContextMenu, {
+  classNames: ['test-demo-context-menu'],
+  classNameBindings: ['object.selected:test-demo-context-menu--selected'],
+  contextSelection: [],
+
+  contextItems: computed('object.selected', function() {
+    if (this.get('object.selected')) {
+      return [
+        { label: 'No action' },
+        { label: 'Singe object action',
+          disabled(objects) {
+            return objects.length > 1;
+          },
+          action() { }
+        },
+        {
+          label: 'Multi actions',
+          subActions: [
+            {
+              label: 'Set to 5',
+              action(objects) { setTo(objects, 5); }
+            },
+            {
+              label: 'Add',
+              subActions: [
+                { label: 'Add 1', action(objects) { increment(objects, 1); } },
+                { label: 'Add 3',
+                  disabled: true,
+                  action(objects) { increment(objects, 3); }
+                },
+                { label: 'Add 10', action(objects) { increment(objects, 10); } }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Reset to 0',
+          icon: 'undo',
+          action(objects) { setTo(objects, 0); }
+        }
+      ];
+    }
+
+    return [];
+  })
+});
