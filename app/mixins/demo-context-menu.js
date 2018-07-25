a4ec5ee93d852a9c535e33b068e11a78e461a9bd
Oleg G.Kapranov <lugatex@yahoo.com>
2018-07-25 20:23:59 +0300

    created mixins - demo-context-menu

--- app/mixins/demo-context-menu.js
+++ app/mixins/demo-context-menu.js
@@ -1,4 +1,22 @@
 import Mixin from '@ember/object/mixin';
+import invokeAction from 'ember-invoke-action';
+import service from '@ember/service/inject';
+import get from '@ember/object/get';
 
 export default Mixin.create({
+  demoContextMenuService: service('demo-context-menu'),
+
+  demoContextMenu(e) {
+    invokeAction(this, '_demoContextMenu', e);
+
+    let demoContextMenu = get(this, 'demoContextMenuService');
+    let items = get(this, 'contextItems');
+    let selection = get(this, 'contextSelection');
+    let details = get(this, 'contextDetails');
+
+    if (items && get(items, 'length')) {
+      e.preventDefault();
+      demoContextMenu.activate(e, items, selection, details);
+    }
+  }
 });
